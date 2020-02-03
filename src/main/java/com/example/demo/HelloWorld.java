0bfc419f0095d939d7cbfeaea2c793e2ea81a9a1
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-02-03 21:00:04 +0100

    Added addition, multiplication and division

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -22,4 +22,57 @@ public class HelloWorld {
 		return sb.reverse().toString();
 	}
 
+	@RequestMapping(value = "/addition", method = RequestMethod.GET)
+	public String addition(String number1, String number2) {
+		int n1 = 0;
+		int n2 = 0;
+		
+		try {
+			n1 = Integer.parseInt(number1);
+			n2 = Integer.parseInt(number2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return n1 + " + " + n2 + " = " + (n1 + n2);
+	}
+	@RequestMapping(value = "/multiplication", method = RequestMethod.GET)
+	public String multiplication(String number1, String number2) {
+		int n1 = 0;
+		int n2 = 0;
+		
+		try {
+			n1 = Integer.parseInt(number1);
+			n2 = Integer.parseInt(number2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return n1 + " * " + n2 + " = " + (n1 * n2);
+	}
+	@RequestMapping(value = "/division", method = RequestMethod.GET)
+	public String division(String number1, String number2) {
+		double n1 = 0;
+		double n2 = 0;
+		double result = 0;
+		String textToReturn = "";
+		
+		try {
+			n1 = Double.parseDouble(number1);
+			n2 = Double.parseDouble(number2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if (n2 != 0) {
+//			textToReturn += "n2 != 0\n";
+			result = n1 / n2;
+		}
+		
+//		textToReturn += "number1: " + number1 + "\n";
+//		textToReturn += "number2: " + number2 + "\n";
+//		textToReturn += "result: " + result + "\n";
+		
+		return textToReturn + n1 + " / " + n2 + " = " + result;
+	}
 }
